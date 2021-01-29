96b78dac3f07d43257a9e47b5353bbd2df3e0ba5
Vlad Ghimboasa <vladalexandrughimboasa@gmail.com>
2021-01-29 12:35:44 -0500

    verification client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,15 +1,66 @@
 import java.io.DataInputStream;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 
 public class Client 
 {
 	private static Socket socket;
 	
+	
+	private static final Pattern PATTERN = Pattern.compile(
+			"^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$"
+	);
+	
+	public static boolean validateIpAddress(final String ipAdress) {
+		return PATTERN.matcher(ipAdress).matches();
+	}
 	/*
 	 * Application Client
 	 */
+	public static void askIp() {
+		Scanner userInput = new Scanner(System.in);
+		
+		String ipAdress = "";
+		boolean inputValid = false;
+		
+		System.out.print("Veulliez entrer l'adress IP \n");
+		
+		while(!inputValid) {
+			ipAdress = userInput.nextLine();
+			inputValid = validateIpAddress(ipAdress);
+		}
+		
+		userInput.close();
+		System.out.print(ipAdress);
+	}
+	
+	public static void askPort() {
+		Scanner userInput = new Scanner(System.in);
+		
+		int port = 0;
+		boolean inputValid = false;
+		
+		System.out.print("Veuillez entrer le port \n");
+		
+		while(!inputValid) {
+			port = userInput.nextInt();
+			if(port >=  5000 && port <= 5050) {
+				inputValid = true;
+			}
+		}	
+
+		userInput.close();
+		System.out.print(inputValid);
+	} 
+	
 	public static void main(String[] args) throws Exception
 	{
+		askIp();
+	
+		askPort();
+		
 		// Adresse et port du serveur
 		String serverAddress = "127.0.0.1";
 		int port = 5000;
